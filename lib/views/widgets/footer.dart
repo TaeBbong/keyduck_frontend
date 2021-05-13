0c3aee6aec857687951688438d2f8f3a51915e41
TaeBbong <mok05289@korea.ac.kr>
2021-05-13 21:12:25 +0900

    [+] Add Footer

--- lib/views/widgets/footer.dart
+++ lib/views/widgets/footer.dart
@@ -3,13 +3,30 @@ import 'package:flutter/material.dart';
 class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        children: [
+          Text('Copyright 밍기원정대, 2021 ALL RIGHTS RESERVED'),
+          Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          Text('Designed by TaeBbong'),
+        ],
+      ),
+    );
   }
 }
 
 class MobileFooter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Text('Copyright 밍기원정대, 2021 ALL RIGHTS RESERVED'),
+          Text('Designed by TaeBbong'),
+        ],
+      ),
+    );
   }
 }
